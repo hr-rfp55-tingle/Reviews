78f63cdfd7fbd3326565e5f076d481f56c59410f
Cora Durham <cora5du@gmail.com>
2021-10-06 22:17:43 -0500

    Added failures to routes

--- server/k6.js
+++ server/k6.js
@@ -3,12 +3,12 @@ import { sleep } from 'k6';
 
 export const options = {
   vus: 1000,
-  duration: '60s',
+  duration: '30s',
 };
 
 export default function () {
-  // const product = Math.floor(Math.random() * 100000 + 900000);
-  const review = Math.floor(Math.random() * 100000 + 4700000);
+  const product = Math.floor(Math.random() * 100000 + 900000);
+  // const review = Math.floor(Math.random() * 100000 + 4700000);
   const params = { headers: { 'Content-Type': 'application/json' } };
 //   const body = JSON.stringify({
 //     "product_id":1,
@@ -21,9 +21,9 @@ export default function () {
 //     "photos": [],
 //     "characteristics": { "14": 5, "15": 5}
 //   });
-  // http.request('GET', `http://localhost:3002/reviews?product_id=${product}`, params);
+  http.request('GET', `http://localhost:3002/reviews?product_id=${product}`, params);
   // http.request('GET', `http://localhost:3002/reviews/meta?product_id=${product}`, params);
-  http.request('PUT', `http://localhost:3002/reviews/${review}/helpful`, params);
+  // http.request('PUT', `http://localhost:3002/reviews/${review}/helpful`, params);
   // http.request('PUT', `http://localhost:3002/reviews/${review}/report`, params);
   // http.request('POST', `http://localhost:3002/reviews`, body, params);
 
